14d68b59b55627bde133393be502200fbc0df77e
FullViper2406 <119512140+FullViper2406@users.noreply.github.com>
2023-09-01 18:43:42 -0300

    frontendatt

--- TelaLogin/indexVideo.php
+++ TelaLogin/indexVideo.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="estilo.css/estilo1.css">
+    <link rel="stylesheet" href="estilo.css/estilo2.css">
     <title>Visualização</title>
     <h1>Seus vídeos</h1><br>
     <style>
