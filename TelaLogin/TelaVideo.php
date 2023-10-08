ddaa780db209330f59069a55da5a317b8f883e02
FullViper2406 <119512140+FullViper2406@users.noreply.github.com>
2023-10-08 17:11:31 -0300

    boobstrapInicio

--- TelaLogin/TelaVideo.php
+++ TelaLogin/TelaVideo.php
@@ -3,18 +3,42 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+ <!-- Make sure you put this AFTER Leaflet's CSS -->
+ <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+ integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+crossorigin=""></script>
     <title>Visualização</title>
-    <style>
-        body {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-wrap: wrap;
-            min-height: vh;
-        }
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+ 
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="Localizacao.php">Home</a>
+
+        </li>
+        <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="index.php">Sair</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="TelaVideo.php">Video</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="Video.php">Upload</a>
+
+        </li>
+       
+      </form>
+    </div>
+  </div>
+</nav>    <style>
+      
         video {
-            width: 640px;
-            height: 360px;
+            width: 300px;
+            height: 400px;
         }
     </style>
 </head>
@@ -74,10 +98,7 @@
         echo "<h1>Faça login para ver os vídeos.</h1>";
     }
     ?>
-    <form action="Video.php" method="post">
-    <input type="submit" name="upload" value="Upload">
-    <button type="button" onclick="window.location.href = 'Localizacao.php';">Voltar</button>
-    </form>
+ 
 </body>
 </html>
 
